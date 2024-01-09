6ab7e76e6bb6bec0c79394d61de22e169abe3010
alokmotion <alokpatel406@gmail.com>
2024-01-09 09:20:16 +0530

    update

--- JAVA2024/src/com/learn/Arrays/CheckSortedArray.java
+++ JAVA2024/src/com/learn/Arrays/CheckSortedArray.java
@@ -10,7 +10,7 @@ public class CheckSortedArray {
 		boolean result = false;
 		
 		
-//		///////////////////
+//		//////////////////
 		
 //		for(int i = 0; i<=arr.length-2 ; i++) {
 //			for(int j=i+1; j<=arr.length-1 ; j++){
