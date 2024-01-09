67da3db70cbf422b165e3d2330816fc655722ada
alokmotion <alokpatel406@gmail.com>
2024-01-09 09:18:32 +0530

    2nd method

--- JAVA2024/src/com/learn/Arrays/CheckSortedArray.java
+++ JAVA2024/src/com/learn/Arrays/CheckSortedArray.java
@@ -5,20 +5,39 @@ public class CheckSortedArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] arr = {1, 2, 3, 4, 5};
+		int[] arr = {1, 2, 6, 4, 5};
 		
-		boolean result = true;
+		boolean result = false;
 		
-		for(int i = 0; i<=arr.length-2 ; i++) {
-			for(int j=i+1; j<=arr.length-1 ; j++){
-				if(arr[i] > arr[j]) {
-					result = false;
-					break;
-					
-					
-				} 
-				
-			}	
+		
+//		///////////////////
+		
+//		for(int i = 0; i<=arr.length-2 ; i++) {
+//			for(int j=i+1; j<=arr.length-1 ; j++){
+//				if(arr[i] > arr[j]) {
+//					result = false;
+//					break;
+//					
+//					
+//				} 
+//				
+//			}	
+//			
+//		}
+//		if(result) {
+//			System.out.println(1);
+//		} else {
+//			System.out.println(0);
+//		}
+		
+//		///////////////////////
+		
+		
+		for(int i =0; i<arr.length-1; i++) {
+			if (arr[i] < arr[i+1]) {
+				result = true;
+				break;
+			} 
 			
 		}
 		if(result) {
